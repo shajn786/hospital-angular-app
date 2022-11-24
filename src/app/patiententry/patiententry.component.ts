98c7856fcfd5fd969d5a4b99b31d51c086230dbc
shajn786 <shajn786@gmail.com>
2022-11-24 10:58:54 +0530

    patiententry

---
+++ src/app/patiententry/patiententry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patiententry',
+  templateUrl: './patiententry.component.html',
+  styleUrls: ['./patiententry.component.css']
+})
+export class PatiententryComponent {
+
+}
