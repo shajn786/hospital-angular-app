5562a9d850e15b60fc7f330e8cb86cad24d010db
shajn786 <shajn786@gmail.com>
2022-11-24 12:00:20 +0530

    patient delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PatiententryComponent } from './patiententry/patiententry.component';
 import { PatientSearchComponent } from './patient-search/patient-search.component';
+import { PatientDeleteComponent } from './patient-delete/patient-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PatiententryComponent,
-    PatientSearchComponent
+    PatientSearchComponent,
+    PatientDeleteComponent
   ],
   imports: [
     BrowserModule,
